d43f6a475e4f6a113389ea788c0b4cad63906486
mayankrd <mayank.dyl@gmail.com>
2017-09-27 13:58:16 -0400

    express js added

--- back-end/app.js
+++ back-end/app.js
@@ -4,10 +4,32 @@
 const express = require('express')
 const app = express()
 
+const elSearch = require('./elastic-search/search_all.js');
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
+})
+
+app.get('/:q', function (req, res) {
+
+    //console.log(elSearch.searchAll(req.params.q));
+
+    //res.send(elSearch.searchAll(req.params.q));
+
+    elSearch.searchAll(req.params.q)
+        .then(results => {
+        // console.log(`found ${results.hits.total} items in ${results.took}ms`);
+        // console.log(`returned article titles:`);
+        // results.hits.hits.forEach((hit, index) => {
+        // console.log(`${hit._source.text} ${hit._source.user_id}`);
+        // resArr.push({ "text": hit._source.text });
+        res.send(results);
+    })
+        .catch(console.error);
 })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+
--- back-end/elastic-search/search_all.js
+++ back-end/elastic-search/search_all.js
@@ -1,5 +1,8 @@
-(function () {
+exports.searchAll = function (queryStr) {
     'use strict';
+
+    var resArr = [{ "test": "test val"}];
+
 
     const elasticsearch = require('elasticsearch');
     const esClient = new elasticsearch.Client({
@@ -13,32 +16,44 @@
 
     // only for testing purposes
     // all calls should be initiated through the module
-    const test = function test() {
+   // const test = function test() {
         let body = {
             size: 20,
             from: 0,
             query: {
                 match: {
                     text: {
-                        query: 'President'
+                        query: queryStr
                     }
                 }
             }
         };
 
         console.log(`retrieving all documents (displaying ${body.size} at a time)...`);
-        search('library', body)
-            .then(results => {
-            console.log(`found ${results.hits.total} items in ${results.took}ms`);
-        console.log(`returned article titles:`);
-        results.hits.hits.forEach((hit, index) => console.log(`${hit._source.text} ${hit._source.user_id}`));
-    })
-        .catch(console.error);
-    };
 
-    test();
+        // search('library', body)
+        //     .then(results => {
+        //     console.log(`found ${results.hits.total} items in ${results.took}ms`);
+        //     console.log(`returned article titles:`);
+        //     results.hits.hits.forEach((hit, index) => {
+        //     console.log(`${hit._source.text} ${hit._source.user_id}`);
+        //     resArr.push({ "text": hit._source.text });
+        // })
+        //console.log("RESULT ARRAY ******** ", resArr);
 
-    module.exports = {
-        search
-    };
-} ());
+        return search('library', body);
+
+
+    //})
+           // .catch(console.error);
+   // };
+
+  //  test();
+
+    // module.exports = {
+    //     search
+    // };
+
+    console.log("RESULT ARRAY AGAIN ******** ", resArr);
+
+};
