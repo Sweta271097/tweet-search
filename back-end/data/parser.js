221b8d148ee330805e8386d12a2c49addba8bdf3
mayankrd <mayank.dyl@gmail.com>
2017-09-28 00:57:31 -0400

    added data parser

--- back-end/data/parser.js
+++ back-end/data/parser.js
@@ -1,3 +1,39 @@
 /**
  * Created by mayankrd on 9/27/17.
  */
+const fs = require('fs');
+
+var result = [];
+
+var jsonString = [];
+
+fs.readFile('assignment.txt', 'utf8', function(err,data) {
+    if(err) throw err;
+    var splitted = data.toString().split("\n");
+
+    console.log(splitted);
+    console.log(splitted.length);
+    console.log(splitted[2]);
+
+    for (var i = 0; i < splitted.length; i++) {
+
+       var obj = { created_at: "", text: "", user_id: "" };
+       var line = splitted[i];
+       obj.created_at = line.substr(0, 19);
+       obj.user_id = line.substr(line.length-7);
+
+       result.push(obj);
+    }
+
+    jsonString = JSON.stringify(result);
+
+    console.log(jsonString);
+});
+
+fs.writeFile("./matrixtest.json", JSON.stringify(jsonString), function(err) {
+
+});
+
+
+
+
